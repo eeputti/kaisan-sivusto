30cb3843c5d9d0edd522cc5f1f5b019c7dd015a7
eeputti <eelispuro@gmail.com>
2026-01-30 23:10:20 +0200

    Remove muistot page title

--- app/muistot/page.tsx
+++ app/muistot/page.tsx
@@ -1,12 +1,10 @@
 import { LayoutFrame } from "@/components/LayoutFrame";
-import { RetroBox } from "@/components/RetroBox";
 import { site } from "@/lib/site";
 
 export default function MuistotPage() {
   return (
     <LayoutFrame active="/muistot">
       <div className="galleryPage">
-        <RetroBox title={site.gallery.title} />
         <div className="galleryGrid">
           {site.gallery.photos.map((photo) => (
             <div key={photo.src} className="card galleryCard">
